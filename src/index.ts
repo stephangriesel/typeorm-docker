1d5ddf2fd46bc48bed5080e536ea2fade7dbb6df
Stephan Griesel <stephangriesel@peopleintouch.com>
2023-06-07 11:10:20 +0200

    initial set up + error handling

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import * as express from "express"
+import * as bodyParser from "body-parser"
+import { Request, Response } from "express"
+import { AppDataSource } from "./data-source"
+import { Routes } from "./routes"
+
+function handleError(err, req, res, next){
+    res.status(err.statusCode || 500).send(err.message)
+}
+
+AppDataSource.initialize().then(async () => {
+
+    // create express app
+    const app = express()
+    app.use(bodyParser.json())
+
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, async(req: Request, res: Response, next: Function) => {
+            try {
+                const result = await (new (route.controller as any))[route.action](req, res, next)
+                res.json(result)
+            }catch(error){
+                next(error)
+            }
+        })
+    })
+
+    app.use(handleError)
+    app.listen(3000)
+
+    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+
+}).catch(error => console.log(error))
